18d66572b3e083e60c7d27e2e01324a38e661dd9
Kacper <kacper.tylec1999@gmail.com>
2023-06-19 18:41:59 +0200

    singleton pattern

---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using Singleton;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Expected behaviour - singleton will be created (console will print "instance created")
+        SingletonClass singletonInstance1 = SingletonClass.CreateInstance();
+
+        
+
+        // The expected behaviour is that singletonInstance2 will not be created (console will print "instance already exist"
+        SingletonClass singletonInstance2 = SingletonClass.CreateInstance();
+        
+
+    }
+}
---
+++ Creational/Singleton/SingletonClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class SingletonClass
+    {
+        //This variable is going to store the Singleton Instance
+        private static SingletonClass? Instance = null;
+        //The following Static Method is going to return the Singleton Instance
+        public static SingletonClass CreateInstance()
+        {
+            //If the variable instance is null, then create the Singleton instance 
+            //else return the already created singleton instance
+            //This version is not thread safe
+            if (Instance == null)
+            {
+                Instance = new SingletonClass();
+                Console.WriteLine("Instance created!");
+            }
+            else
+            {
+                Console.WriteLine("Instance already exist");
+            }
+            
+            //Return the Singleton Instance
+            return Instance;
+        }
+        //Constructor is Private means, from outside the class we cannot create an instance of this class
+        private SingletonClass()
+        {
+            
+        }
+    }
+}
